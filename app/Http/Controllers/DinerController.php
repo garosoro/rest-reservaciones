ec5d5afaaabd0a7cd1258cca7f540bca4f04bb2a
garosoro <gabrielr.sotorojas@gmail.com>
2025-04-18 14:05:51 -0500

    Build: creation of Table,Dinner and Reservation files

---
+++ app/Http/Controllers/DinerController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Diner;
+use Illuminate\Http\Request;
+
+class DinerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Diner $diner)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Diner $diner)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Diner $diner)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Diner $diner)
+    {
+        //
+    }
+}
